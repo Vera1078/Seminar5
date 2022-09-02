b585c3072452a9d84eb8b77708c36641639b31f0
Vera Lippgardt <vera1078@mail.ru>
2022-09-02 23:05:47 +0300

    Замена +элемента массива на -элемент и наоборот; присутствует ли заданное число

---
+++ zadacha3233/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] EnterRandomArray()
+{
+    Console.Write("Enter length of array...\t");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = new int[number];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+
+    return array;
+}
+
+void ChangeElements(int[] array)
+{
+    Console.WriteLine($"You array:       {String.Join(", ", array)}");
+    
+    for (int j = 0; j < array.Length; j++)
+    {
+        array[j] = -1 * array[j];
+    }
+
+    Console.WriteLine($"You revert array: {String.Join(", ", array)}");
+}
+
+
+bool IsInArray(int[] array)
+{
+    Console.Write("Enter a number for find in array...\t");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"You array: {String.Join(", ", array)}");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true;
+    }
+
+    return false;
+}
+
+ChangeElements(EnterRandomArray());
+
+Console.WriteLine(IsInArray(EnterRandomArray()));
+
+
+// // _______________________________________________________
+// // Решение Маргариты
+// void NumberSearch()
+// {
+
+// int number = new Random().Next(1,17);
+
+// int num = new Random().Next(17);
+// int[] array = new int[num];
+
+// bool search = false;
+
+// for(int i=0;i < array.Length; i++)
+// {
+//     array[i]=new Random().Next(17);
+//     if(number == array[i])
+//     search = true;
+// }
+// Console.WriteLine($"{number}; array [{string.Join(",",array)}]->{(search?"да":"нет")}");
+// }
+
+// NumberSearch();
